a6b2164b8515077e86914040e6da96ca5e284767
Jakob Grøtan Gregusson <jakobgg@stud.ntnu.no>
2023-02-10 15:09:39 +0100

    Connected Firebase to gitlab project
    Co-authored-by: Emmanuel and Elin

--- journey_frontend/src/components/App.js
+++ journey_frontend/src/components/App.js
@@ -1,5 +1,7 @@
 import logo from '../logo.svg';
 import '../styles/App.css';
+import { db } from '../firebase-config';
+import { getDatabase, ref, set } from "firebase/database";
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
         </p>
         <a
           className="App-link"
---
+++ journey_frontend/src/firebase-config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from 'firebase/database';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyArmgE8SVG3CYdDZ_tdkQJL3oY-29zHags",
+  authDomain: "journey-5ea10.firebaseapp.com",
+  projectId: "journey-5ea10",
+  storageBucket: "journey-5ea10.appspot.com",
+  messagingSenderId: "118588368280",
+  appId: "1:118588368280:web:848730ebe73396c04fb7ed",
+  measurementId: "G-T3VSPM6QS2",
+  databaseURL: "https://journey-5ea10-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
