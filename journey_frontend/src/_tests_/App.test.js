7d9e61ef63cbbd06f0be39b284b10b9a5a37c43c
Emmanuel Odai Quarcoo <emmanueq@stud.ntnu.no>
2023-02-13 21:40:14 +0100

    Revert "Merge branch '25-setup-homepage' into 'main'"
    This reverts merge request !3

--- journey_frontend/src/_tests_/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen, cleanup } from '@testing-library/react';
-import App from '../components/App';
-
-test('App contents should render', () => {
-  render(<App/>)
-  const appElement = screen.getByTestId('main-content')
-  expect(appElement).toBeInTheDocument();
-})
-
--- journey_frontend/src/_tests_/Navbar.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen, cleanup } from '@testing-library/react';
-import App from '../components/App';
-import { Navbar } from '../components/NavBar';
-
-test('Navbar should render', () => {
-    render(<App />);
-    const navbar = screen.getByTestId('navbar');
-    expect(navbar).toBeInTheDocument();
-})
