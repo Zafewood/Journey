90efc9004f38de7d87a5a2728af58795c362c566
ecbjerve <ecbjerve.stud.ntnu.no>
2023-02-17 12:52:29 +0100

    Added functionality to the login page. Now it connects to the firebase

--- journey_frontend/src/pages/LoginPage.jsx
+++ journey_frontend/src/pages/LoginPage.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import { auth } from '../firebase-config';
 
 function LoginPage() {
+    const[email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+
+    const signIn = (e) => { 
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => { 
+            console.log(userCredential);
+        }).catch((error) => { 
+            console.log(error);
+        })
+    }
+
   return (
     <>
+    <form onSubmit={signIn}>
         <h1>Log In</h1><br />
         <label>Email</label><br />
-        <input></input><br /><br />
+        <input type="email" value={email} onChange = {(e) => setEmail(e.target.value)}></input><br /><br />
         <label>Password</label><br />
-        <input></input><br /><br />
+        <input type="password" value={password} onChange = {(e) => setPassword(e.target.value)}></input><br /><br />
         <button>LOG IN</button><br /><br /><br />
         <h4>Don´t have an account??</h4>
         <button><Link to='/createuserpage'>Create new account</Link></button>
-
+    </form>
     </>
   )
 }
