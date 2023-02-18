22f78f283b559090aee3aec36a2b7ff551263518
quarc0o <emmanuelq150@gmail.com>
2023-02-18 15:10:50 +0100

    Card component for homepage
    Made simple card components for trips on homepage with ability to expan
    in order to show trip comments. Also changed homepage css layout.

---
+++ journey_frontend/src/components/Trips/UserComment.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import defaultUser from '../../assets/default_user.jpeg'
+import '../../styles/Trips/UserComment.css'
+
+function UserComment() {
+  return (
+    <div className='comment-content'>
+        <div className='comment-left'>
+            <img src={defaultUser} alt="" className='default-user-img'/>
+        </div>
+        <div className='comment-right'>
+            <h3>Name</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Neque deserunt delectus placeat quo non veritatis accusamus 
+                tempore distinctio nostrum odio molestiae, voluptatum commodi? 
+                Labore dolores est beatae aliquam velit vitae?
+            </p>
+            <p>Rating</p>
+        </div>
+    </div>
+  )
+}
+
+export default UserComment
