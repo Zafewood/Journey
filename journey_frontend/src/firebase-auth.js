c92ac10db1da742523b11ec6f1d0fbafcda8a145
ecbjerve <ecbjerve.stud.ntnu.no>
2023-02-20 08:58:15 +0100

    Added name into the database
    Added functionality to add a name into the database, and also input field

--- journey_frontend/src/firebase-auth.js
+++ journey_frontend/src/firebase-auth.js
@@ -3,13 +3,14 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { db } from './firebase-config';
 import { set, ref } from 'firebase/database';
 
-const createUserWithCredentials = (mail, password) => {
+const createUserWithCredentials = (mail, password, displayName = null) => {
     createUserWithEmailAndPassword(auth, mail, password)
       .then((userCredential) => {
         // Signed in 
         const user = userCredential.user;
         set(ref(db, 'users/' + user.uid), {
-          email: user.email
+          email: user.email,
+          displayName: displayName,
         })
       })
       .catch((error) => {
--- journey_frontend/src/pages/CreateUserPage.jsx
+++ journey_frontend/src/pages/CreateUserPage.jsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
 import { auth } from '../firebase-config'
+// import { getAuth, updateProfile } from '../firebase/auth';
 import createUserWithCredentials from '../firebase-auth';
 import { React, useState } from 'react';
 import { forceLongPolling } from 'firebase/database';
+import { updateProfile } from 'firebase/auth';
 
 
 function CreateUserPage() {
@@ -10,6 +12,8 @@ function CreateUserPage() {
   // in respective input fields 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
 
   const updateEmail = (event) => {
     setEmail(event.target.value)
@@ -19,15 +23,27 @@ function CreateUserPage() {
     setPassword(event.target.value)
   }
 
+  const updateUsername = (event) => { 
+    setUsername(event.target.value);
+  }
+  
+  
   const createUser = () => {
-    createUserWithCredentials(email, password)
+    createUserWithCredentials(email, password, username)
+    updateProfile(auth.currentUser, { 
+        displayName: username
+    })
     console.log('mail: ', email);
     console.log('pass: ', password);
+    console.log ('username: ', username);
+    console.log('displayname: ' + auth.currentUser.displayName + "");
   }
 
   return (
     <>
         <h1>Create User</h1><br />
+        <label> Username </label> <br/>
+        <input onChange={updateUsername} ></input> <br/> <br/>
         <label>Enter Email</label><br />
         <input onChange={updateEmail}></input><br /><br />
         <label>Choose Password</label><br />
@@ -39,6 +55,4 @@ function CreateUserPage() {
   )
 }
 
-
 export default CreateUserPage
-
