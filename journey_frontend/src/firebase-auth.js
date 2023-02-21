b024bba43d14580d74bb2b8b9a5f082aefe01c24
Jakob Grøtan Gregusson <jakobgg@stud.ntnu.no>
2023-02-21 14:06:09 +0100

    Added home country and navigation to login
    Added the ability for users to add their home country
    when creating a user. Made the page navigate to loginpage whenever
    a succesful user was made

--- journey_frontend/src/firebase-auth.js
+++ journey_frontend/src/firebase-auth.js
@@ -3,21 +3,23 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { db } from './firebase-config';
 import { set, ref } from 'firebase/database';
 
-const createUserWithCredentials = (mail, password, displayName = null) => {
-    createUserWithEmailAndPassword(auth, mail, password)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        set(ref(db, 'users/' + user.uid), {
-          email: user.email,
-          displayName: displayName,
+const createUserWithCredentials = (mail, password, displayName = null, homeCountry = null) => {
+    return new Promise((resolve, reject) => {
+      createUserWithEmailAndPassword(auth, mail, password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          set(ref(db, 'users/' + user.uid), {
+            email: user.email,
+            displayName: displayName,
+            homeCountry: homeCountry,
+          })
+          resolve(true);
         })
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-      });
-    }
+        .catch((error) => {
+          resolve(false);
+        });
+    });
+  }
 
 export default createUserWithCredentials;
--- journey_frontend/src/pages/CreateUserPage.jsx
+++ journey_frontend/src/pages/CreateUserPage.jsx
@@ -1,19 +1,15 @@
-import { Link } from 'react-router-dom';
-import { auth } from '../firebase-config'
-// import { getAuth, updateProfile } from '../firebase/auth';
+import { Link, useNavigate } from 'react-router-dom';
 import createUserWithCredentials from '../firebase-auth';
 import { React, useState } from 'react';
-import { forceLongPolling } from 'firebase/database';
-import { updateProfile } from 'firebase/auth';
 
 
 function CreateUserPage() {
-  // email and password state variables update with each keypress
+  // email, password, username and home country state variables update with each keypress
   // in respective input fields 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
-
+  const [homeCountry, setHomeCountry] = useState("");
 
   const updateEmail = (event) => {
     setEmail(event.target.value)
@@ -27,16 +23,19 @@ function CreateUserPage() {
     setUsername(event.target.value);
   }
   
+  const updateHomeCountry = (event) => { 
+    setHomeCountry(event.target.value);
+  }
+
+  const navigate = useNavigate();
   
   const createUser = () => {
-    createUserWithCredentials(email, password, username)
-    updateProfile(auth.currentUser, { 
-        displayName: username
-    })
-    console.log('mail: ', email);
-    console.log('pass: ', password);
-    console.log ('username: ', username);
-    console.log('displayname: ' + auth.currentUser.displayName + "");
+    const result = createUserWithCredentials(email, password, username, homeCountry)
+    result.then((data) => {
+      if (data) {
+        navigate('../loginpage');
+      }
+    });
   }
 
   return (
@@ -44,6 +43,8 @@ function CreateUserPage() {
         <h1>Create User</h1><br />
         <label> Username </label> <br/>
         <input onChange={updateUsername} ></input> <br/> <br/>
+        <label> Home Country </label> <br/>
+        <input onChange={updateHomeCountry} ></input> <br/> <br/>
         <label>Enter Email</label><br />
         <input onChange={updateEmail}></input><br /><br />
         <label>Choose Password</label><br />
