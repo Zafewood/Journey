4c8e03f84528e55a34137f16a054d55961b2c6bc
amundbba <amundbba@stud.ntnu.no>
2023-03-06 12:00:17 +0100

    Added Price and Keywords
    Added textfields for price and keywords for all trips.
    
    Co author Emmanuel

--- journey_frontend/src/components/Trips/CreateTrip.jsx
+++ journey_frontend/src/components/Trips/CreateTrip.jsx
@@ -12,8 +12,10 @@ function TripsCard({ tripAddedHandler }) {
     const [trip, setTrip] = useState({
         tripTitle: '',
         tripDuration: '',
+        tripPrice: '',     
         tripCountry: '',
         tripCity: '',
+        tripKeywords: '',   
         tripDescription: ''
       });
 
@@ -57,6 +59,10 @@ function TripsCard({ tripAddedHandler }) {
     const updateDuration = (event) => {
         setTrip(prevTrip => ({ ...prevTrip, tripDuration: event.target.value }));
     };
+
+    const updatePrice = (e) => {
+        setTrip(prevTrip => ({ ...prevTrip, tripPrice: e.target.value }));
+    };
     
     const updateCountry = (e) => {
         setTrip(prevTrip => ({ ...prevTrip, tripCountry: e.target.value }));
@@ -64,6 +70,10 @@ function TripsCard({ tripAddedHandler }) {
     
     const updateCity = (e) => {
         setTrip(prevTrip => ({ ...prevTrip, tripCity: e.target.value }));
+    };
+
+    const updateKeywords = (e) => {
+        setTrip(prevTrip => ({ ...prevTrip, tripKeywords: e.target.value }));
     };
     
     const updateDescription = (e) => {
@@ -84,10 +94,14 @@ function TripsCard({ tripAddedHandler }) {
                     <label id='authorLabel'> Author </label> 
                     <label htmlFor='duationInput'> Duration (days) </label> 
                     <input id='duationInput' type="integer" onChange = {updateDuration}></input> 
+                    <label htmlFor="priceInput">Estimated Price</label>
+                    <input id="priceInput" type="integer" onChange={updatePrice}></input>
                     <label htmlFor='countryInput'> Country/Countries </label> 
                     <input id='countryInput' type ="text" onChange={updateCountry}></input> 
                     <label htmlFor='citiesInput'> City/Cities </label> 
                     <input id='citiesInput' type ="text" onChange={updateCity}></input> 
+                    <label htmlFor='keywordsInput'> Keywords </label> 
+                    <input id='keywordsInput' type ="text" onChange={updateKeywords}></input>
                     <label htmlFor='descriptionInput' > Description </label> 
                     <textarea id='descriptionInput' type ="text" onChange={updateDescription} textarea rows={5} cols={40} ></textarea> 
                 </div>
