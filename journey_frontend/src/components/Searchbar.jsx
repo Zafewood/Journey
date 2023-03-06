f907844dd70c86210db7a15c899387acec175dc8
quarc0o <emmanuelq150@gmail.com>
2023-03-06 11:55:14 +0100

    basic sorting functionality

--- journey_frontend/src/components/Searchbar.jsx
+++ journey_frontend/src/components/Searchbar.jsx
@@ -1,23 +1,38 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import '../styles/Searchbar.css';
 
-export default function Searchbar ({ handleSearch }) { 
-    const [searchValue, setSearchValue] = ("");
+export default function Searchbar ({ handleSearch, handleSort }) { 
+    const [searchValue, setSearchValue] = (""); 
+    const [sortingBy, setSortingBy] = useState("Newest");
+    
+    useEffect(() => {
+        handleSort(sortingBy);
+      }, [handleSort])
 
     const searchbarHandler = (event) => {
         const searchText = event.target.value;
         handleSearch(searchText)
     }
 
+    const sortingHandler = (event) => {
+        const sortValue = event.target.value
+        console.log(sortValue);
+        setSortingBy(sortValue);
+        handleSort(sortValue);
+    }
+    
+
     return (
         <div className="searchbarDiv">
             <input className="searchbar" type="text" placeholder="Search for your trips" value={searchValue} onChange={searchbarHandler}/>
             <button className="searchbutton" type="submit"> Search </button>
-            <select className="filterdropdown" name="filter" id="filter" value="Select filter" onChange={() => {}}>
-                <option value="Select filter" disabled hidden>Select filter</option>
-                <option value="rating">Rating</option>
-                <option value="country">Country</option>
-                <option value="duration">Duration</option>
-                <option value="price">Price</option>
+            <select className="filterdropdown" name="filter" id="filter" value={sortingBy} onChange={sortingHandler}>
+                <option value="Newest">Newest first</option>
+                <option value="Rating">Rating</option>
+                <option value="Country">Country</option>
+                <option value="Duration">Duration</option>
+                <option value="Price">Price</option>
             </select>
         </div>
     )
--- journey_frontend/src/pages/HomePage.jsx
+++ journey_frontend/src/pages/HomePage.jsx
@@ -20,8 +20,6 @@ function HomePage({ allTrips, tripAddedHandler, handleUserEditTrip, signedInUser
   }, [allTrips]);
 
   const handleSearch = (searchText) => {
-
-
     const matchingTrips = initialTripsArray.filter((trip) => {
       const { tripTitle, tripCountry, tripCity } = trip;
       return (
@@ -34,6 +32,40 @@ function HomePage({ allTrips, tripAddedHandler, handleUserEditTrip, signedInUser
     setFilteredTrips(matchingTrips);
   }
 
+  const handleSort = (sortBy) => {
+    let sortedTrips;
+    console.log("Sort by: ", sortBy);
+    switch (sortBy) {
+      case "Title":
+        sortedTrips = [...filteredTrips].sort((a, b) =>
+          a.tripTitle.localeCompare(b.tripTitle)
+        );
+        break;
+      case "Duration":
+        console.log("sort by duration");
+        sortedTrips = [...filteredTrips].sort(
+          (a, b) => parseInt(a.tripDuration) - parseInt(b.tripDuration)
+        );
+        break;
+      case "Country":
+        console.log("sort by country");
+        sortedTrips = [...filteredTrips].sort((a, b) =>
+          a.tripCountry.localeCompare(b.tripCountry)
+        );
+        case "Price":
+        console.log("sort by country");
+        sortedTrips = [...filteredTrips].sort((a, b) =>
+          a.tripCountry.localeCompare(b.tripCountry)
+        );
+        break;
+      default:
+        sortedTrips = filteredTrips;
+        break;
+    }
+
+    setFilteredTrips(sortedTrips);
+  }
+
   return (
     <>
       <div className='hero-content'>
@@ -43,7 +75,7 @@ function HomePage({ allTrips, tripAddedHandler, handleUserEditTrip, signedInUser
       
       <div className='bottom-content'>
         <h1 className='text-over-image'>NEWEST TRIPS</h1>
-        <div><Searchbar handleSearch={handleSearch}></Searchbar></div>
+        <div><Searchbar handleSearch={handleSearch} handleSort={handleSort}></Searchbar></div>
         <div className='card-view'>
           <CreateTrip tripAddedHandler={tripAddedHandler}/>
           {filteredTrips.map((tripObject, index) => {
