e7d531fbe8220d3816fc394c27e65ca1cc26c04d
quarc0o <emmanuelq150@gmail.com>
2023-03-17 15:26:52 +0100

    fix

--- journey_frontend/src/services/firebaseService.js
+++ journey_frontend/src/services/firebaseService.js
@@ -3,10 +3,10 @@ import { set, ref, onValue, get, child, update } from 'firebase/database';
 import { uuidv4 } from '@firebase/util';
 
 const getCurrentUserNode = () => {
+  const userID = auth.currentUser.uid;
   const dbRef = ref(db);
-  const currentUserID = auth.currentUser.uid;
   return new Promise((resolve, reject) => {
-    get(child(dbRef, `users/${currentUserID}`)).then((snapshot) => {
+    get(child(dbRef, `users/${userID}`)).then((snapshot) => {
       if (snapshot.exists()) {
         const userNode = snapshot.val();
         resolve(userNode);
